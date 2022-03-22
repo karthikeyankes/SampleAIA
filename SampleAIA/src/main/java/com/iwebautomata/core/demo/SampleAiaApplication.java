88e6a74b1860f881d1faeb51e9dc651b610aa042
Karthikeyan <karthikeyankes@gmail.com>
2022-03-22 19:01:01 +0530

    sample

--- SampleAIA/src/main/java/com/iwebautomata/core/demo/SampleAiaApplication.java
+++ SampleAIA/src/main/java/com/iwebautomata/core/demo/SampleAiaApplication.java
@@ -2,7 +2,7 @@ package com.iwebautomata.core.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//sample
 @SpringBootApplication
 public class SampleAiaApplication {
 
