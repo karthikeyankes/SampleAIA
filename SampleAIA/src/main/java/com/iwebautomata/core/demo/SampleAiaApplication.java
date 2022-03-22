6894fb171a3d7fdf5508ace05c4f12ddffe5050c
Karthikeyan <karthikeyankes@gmail.com>
2022-03-22 18:55:25 +0530

    first commit
    first commit

---
+++ SampleAIA/src/main/java/com/iwebautomata/core/demo/SampleAiaApplication.java
@@ -0,0 +1,13 @@
+package com.iwebautomata.core.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAiaApplication.class, args);
+	}
+
+}
---
+++ SampleAIA/src/test/java/com/iwebautomata/core/demo/SampleAiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwebautomata.core.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
