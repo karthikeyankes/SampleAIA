c9514e3394679381ad97298a6f041fedd0d80c3e
Karthikeyan <karthikeyankes@gmail.com>
2022-03-24 18:16:56 +0530

    fresh batch is commit
    fresh batch is commit

---
+++ AIAISOMX/src/main/java/com/iwebautomata/aia/iso/core/AiaisomxApplication.java
@@ -0,0 +1,13 @@
+package com.iwebautomata.aia.iso.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiaisomxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiaisomxApplication.class, args);
+	}
+
+}
---
+++ AIAISOMX/src/test/java/com/iwebautomata/aia/iso/core/AiaisomxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwebautomata.aia.iso.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiaisomxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
